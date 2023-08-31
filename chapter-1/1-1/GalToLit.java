753577a61c78c27d57dc71e3b02f570761550b67
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2023-08-31 10:28:37 +0100

    exercise 1-1

---
+++ chapter-1/1-1/GalToLit.java
@@ -0,0 +1,16 @@
+/*
+    Try This 1-1
+    This program converts gallons to litres
+    Call this program GalToLit.java
+ */
+class GalToLit {
+    public static void main(String[] args) {
+        double gallons;
+        double litres;
+
+        gallons = 10;
+        litres = 10 * 3.7854;
+
+        System.out.println(gallons + " gallons is " + litres + " litres.");
+    }
+}
