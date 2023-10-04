9ec3fdf4186074d0065ba0c9eaa401e63a5c149c
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2023-10-04 12:15:15 +0100

    did exercise 4-1

---
+++ chapter-4/4-1/HelpClassDemo.java
@@ -0,0 +1,92 @@
+/*
+    Try This 4-1
+    Convert the help system from Try This 3-3 into
+    a Help class
+ */
+class Help {
+    void helpOn(int what) {
+        switch(what) {
+            case '1':
+                System.out.println("The if:\n");
+                System.out.println("if(condition) statement;");
+                System.out.println("else statement;");
+                break;
+            case '2':
+                System.out.println("The switch:\n");
+                System.out.println("switch(expression) {");
+                System.out.println("\tcase constant:");
+                System.out.println("\t\tstatement sequence;");
+                System.out.println("\t\tbreak;");
+                System.out.println("\t//...");
+                System.out.println("}");
+                break;
+            case '3':
+                System.out.println("The for:\n");
+                System.out.println("for(initialisation; condition; iteration)");
+                System.out.println("\t statement;");
+                break;
+            case '4':
+                System.out.println("The while:\n");
+                System.out.println("while(condition) statement;");
+                break;
+            case '5':
+                System.out.println("The do-while:\n");
+                System.out.println("do {");
+                System.out.println("\tstatement;");
+                System.out.println("} while(condition);");
+                break;
+            case '6':
+                System.out.println("The break:\n");
+                System.out.println("break; or break label;");
+                break;
+            case '7':
+                System.out.println("The continue:\n");
+                System.out.println("continue; or continue label;");
+                break;
+
+        }
+        System.out.println();
+    }
+
+    void showMenu() {
+        System.out.println("Help on: ");
+        System.out.println("\t1. if");
+        System.out.println("\t2. switch");
+        System.out.println("\t3. for");
+        System.out.println("\t4. while");
+        System.out.println("\t5. do-while");
+        System.out.println("\t6. break");
+        System.out.println("\t7. continue\n");
+        System.out.print("Choose one (q to quit): ");
+    }
+
+    boolean isValid(int ch) {
+        if (ch < '1' | ch > '7' & ch != 'q') return false;
+        else return true;
+    }
+}
+
+class HelpClassDemo {
+    public static void main(String[] args) throws java.io.IOException {
+        char choice, ignore;
+        Help helpobj = new Help();
+
+        for( ; ; ) {
+            do {
+                helpobj.showMenu();
+
+                choice = (char) System.in.read();
+
+                do {
+                    ignore = (char) System.in.read();
+                } while (ignore != '\n');
+            } while(!helpobj.isValid(choice));
+
+            if(choice == 'q') break;
+
+            System.out.println("\n");
+
+            helpobj.helpOn(choice);
+        }
+    }
+}
