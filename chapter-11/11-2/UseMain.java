efde2adb3766ea522d9da2139175e81ab9b76c29
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2024-02-29 08:10:46 +0000

    Completed exercise 11-2

---
+++ chapter-11/11-2/UseMain.java
@@ -0,0 +1,26 @@
+/*
+    Try This 11-2
+    Controlling the main thread.
+ */
+
+class UseMain {
+    public static void main(String[] args) {
+        Thread thrd;
+
+        thrd  = Thread.currentThread();
+
+        System.out.println("Main thread is called: " + thrd.getName());
+
+        System.out.println("Priority: " + thrd.getPriority());
+
+        System.out.println();
+
+        System.out.println("Setting name and priority.\n");
+        thrd.setName("Thread #1");
+        thrd.setPriority(Thread.NORM_PRIORITY + 3);
+
+        System.out.println("Main thread is now called: " + thrd.getName());
+
+        System.out.println("Priority is now: " + thrd.getPriority());
+    }
+}
